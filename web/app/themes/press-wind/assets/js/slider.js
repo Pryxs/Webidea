5a87eff1db44b8f6cc0c7161fc715d81c1a0b00e
Pryxs <corentin25200@gmail.com>
2023-01-26 19:50:53 +0100

    style(responsive)

--- web/app/themes/press-wind/assets/js/slider.js
+++ web/app/themes/press-wind/assets/js/slider.js
@@ -6,11 +6,27 @@ export default function manageSlider(){
     var swiper = new Swiper(".mySwiper", {
         modules: [Navigation],
 
-        slidesPerView: 3,
-        spaceBetween: 20,
+        slidesPerView: 1,
+        spaceBetween: 10,  
         navigation: {
             nextEl: ".swiper-button-next",
             prevEl: ".swiper-button-prev",
         },
+
+        breakpoints: {  
+            360: {       
+                slidesPerView: 1,
+                spaceBetween: 10     
+            },   
+
+            740: {       
+                slidesPerView: 2,
+                spaceBetween: 15     
+            }, 
+            1450: {       
+                slidesPerView: 3,
+                spaceBetween: 20     
+            }, 
+        }  
     })
 }
